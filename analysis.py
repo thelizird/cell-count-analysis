9bcd53f36aa3d3da044ebdb2507953174496b600
thelizird <owenmendiola@gmail.com>
2026-09-07 20:36:52 -0600

    part 2 analysis

---
+++ analysis.py
@@ -0,0 +1,14 @@
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect('cell_data.db')
+
+df = pd.read_sql("SELECT * FROM samples", conn)
+
+df['total_count'] = df['b_cell'] + df['cd8_t_cell'] + df['cd4_t_cell'] + df['nk_cell'] + df['monocyte']
+
+melted = df.melt(id_vars=['sample_id','total_count'], value_vars=['b_cell', 'cd8_t_cell', 'cd4_t_cell', 'nk_cell', 'monocyte'], var_name='population', value_name='count')
+
+melted['percentage'] = melted['count'] / melted['total_count'] * 100
+
+print(melted.head())
